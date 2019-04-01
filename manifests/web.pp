ac10fe55b97a4c4851f9b03b38692168f5aa1814
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-04-01 09:14:37 -0300

    refactor(web): Fix ident
    Refac code patterns

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,8 +1,8 @@
-exec { "apt-update":
-    command => "/usr/bin/apt-get update"
+exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
 }
 
-package { ["openjdk-7-jre", "tomcat7"]:
-    ensure => installed,
-    require => Exec["apt-update"]
+package { ['openjdk-7-jre', 'tomcat7']:
+    ensure  => installed,
+    require => Exec['apt-update']
 }
